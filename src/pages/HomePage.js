549c4e0029a75bdf3836da88e46415eaafd0221d
Favorjs <107522010+Favorjs@users.noreply.github.com>
2025-12-01 09:36:30 +0100

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -51,7 +51,7 @@ const HomePage = () => {
   const downloadForm = (formType) => {
     // Map form types to their corresponding Cloudinary URLs
     const formFiles = {
-      'Stock Broker Docket': 'https://res.cloudinary.com/apelng/raw/upload/v1762418330/THE_INITIATES_PLC_2025_Right_Brokers_Docket_xwv82r.xls',
+      'Stock Broker Docket': 'https://res.cloudinary.com/apelng/raw/upload/v1764578164/THE_INITIATES_PLC_2025_Right_Brokers_Docket_wurfeu.xls',
       'Dematerialization Form': 'https://res.cloudinary.com/apelng/image/upload/v1762418562/FULL-DEMATERIAL-MIGRATION-FORM-1_1_mmibqe.pdf',
       'Rights Circular': 'https://res.cloudinary.com/apelng/image/upload/v1763988769/TIP_Rights_Circular_ledega_b_zke5hk.pdf',
       'Public Offer': 'https://res.cloudinary.com/apelng/image/upload/v1761666679/TIP_Public_Offer_bvnzju.pdf'
